9ee7a90cc0f043f042e3037ab059e88cabf48d49
coderyu5523 <coderyu5523@naver.com>
2024-04-22 15:40:10 +0900

    환경 설정

---
+++ src/main/java/shop/mtcoding/projoctbodykey/ProjoctBodykeyApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.projoctbodykey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjoctBodykeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjoctBodykeyApplication.class, args);
+    }
+
+}
