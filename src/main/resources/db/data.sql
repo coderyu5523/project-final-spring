a9c7e04df5e5d425cc611160b75495e3728dc40e
coderyu5523 <153602328+coderyu5523@users.noreply.github.com>
2024-05-19 00:00:23 +0900

    [fix] activity dummy

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -9,8 +9,8 @@ VALUES ('ssar', '$2a$10$5Z2/Wx9Lc8jCDSOz9pd6FOSFa3rou4lkf9gzFFrSzddRibYJAafau', 
 
 INSERT INTO activity_tb(user_id, walking, drink_water, created_at)
 VALUES (1, 5325, 2000, '2024-05-16'),
-       (1, 10000, 2000, '2024-05-18'),
        (1, 7028, 2300, '2024-05-17'),
+       (1, 10000, 2000, '2024-05-18'),
        (1, 10000, 2500, '2024-05-19'),
        (1, 10000, 2000, '2024-05-20'),
        (1, 7028, 2300, '2024-05-21'),
