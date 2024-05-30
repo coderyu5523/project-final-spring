404fcdfd8ae8ad78b6a824f8484c5b236fa6f4c1
coderyu5523 <coderyu5523@naver.com>
2024-05-30 21:33:37 +0900

    upload폴더

--- src/main/java/shop/mtcoding/projoctbodykey/_core/IndexController.java
+++ src/main/java/shop/mtcoding/projoctbodykey/_core/IndexController.java
@@ -3,7 +3,7 @@ package shop.mtcoding.projoctbodykey._core;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//aws 헬스체크를 위해 사용
+//aws CI/CD 헬스체크를 위해 필요
 @RestController
 public class IndexController {
 
